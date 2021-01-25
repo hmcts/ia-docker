48a9a1a7d886c47acbc5cc3d65be280415dce74c
Brad Sorour <brad.sorour@gmail.com>
2021-01-25 16:53:38 +0000

    RIA-3959 wiremock transformer

--- bin/wiremock.sh
+++ bin/wiremock.sh
@@ -11,11 +11,20 @@ share_case_a_id="$(curl --silent --show-error -X GET "${IDAM_URL}/details" -H "a
 share_case_b_code="$(sh ./idam-user-token.sh "${TEST_LAW_FIRM_SHARE_CASE_B_USERNAME}" "${TEST_LAW_FIRM_SHARE_CASE_B_PASSWORD}")"
 share_case_b_id="$(curl --silent --show-error -X GET "${IDAM_URL}/details" -H "accept: application/json" -H "authorization: Bearer ${share_case_b_code}" | jq -r .id )"
 
+share_case_org2_code="$(sh ./idam-user-token.sh "${TEST_LAW_FIRM_SHARE_CASE_ORG2_USERNAME}" "${TEST_LAW_FIRM_SHARE_CASE_ORG2_PASSWORD}")"
+share_case_org2_id="$(curl --silent --show-error -X GET "${IDAM_URL}/details" -H "accept: application/json" -H "authorization: Bearer ${share_case_org2_code}" | jq -r .id )"
+
+share_case_c_code="$(sh ./idam-user-token.sh "${TEST_LAW_FIRM_SHARE_CASE_C_USERNAME}" "${TEST_LAW_FIRM_SHARE_CASE_C_PASSWORD}")"
+share_case_c_id="$(curl --silent --show-error -X GET "${IDAM_URL}/details" -H "accept: application/json" -H "authorization: Bearer ${share_case_c_code}" | jq -r .id )"
+
+share_case_d_code="$(sh ./idam-user-token.sh "${TEST_LAW_FIRM_SHARE_CASE_D_USERNAME}" "${TEST_LAW_FIRM_SHARE_CASE_D_PASSWORD}")"
+share_case_d_id="$(curl --silent --show-error -X GET "${IDAM_URL}/details" -H "accept: application/json" -H "authorization: Bearer ${share_case_d_code}" | jq -r .id )"
+
 curl -X POST \
 --data '{
           "request": {
             "method": "GET",
-            "urlPath": "/refdata/external/v1/organisations/users"
+            "urlPath": "/refdata/external/v1/organisations/users_original"
           },
           "response": {
             "status": 200,
@@ -409,7 +418,7 @@ curl -X POST \
 --data '{
           "request": {
             "method": "GET",
-            "urlPath": "/refdata/external/v1/organisations"
+            "urlPath": "/refdata/external/v1/organisations_original"
           },
           "response": {
             "status": 200,
@@ -466,6 +475,42 @@ curl -X POST \
             }
           }
         }' \
+http://localhost:8991/__admin/mappings/new
+
+curl -X POST \
+--data '{
+          "request": {
+            "method": "GET",
+            "urlPath": "/refdata/external/v1/organisations"
+          },
+          "response": {
+            "status": 200,
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "body": "Original body",
+            "transformers": ["body-transformer"]
+          }
+        }
+      }' \
+http://localhost:8991/__admin/mappings/new
+
+curl -X POST \
+--data '{
+          "request": {
+            "method": "GET",
+            "urlPath": "/refdata/external/v1/organisations/users"
+          },
+          "response": {
+            "status": 200,
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "body": "Original body",
+            "transformers": ["body-transformer"]
+          }
+        }
+      }' \
 http://localhost:8991/__admin/mappings/new
 
 # make responses persistent in Docker volume
