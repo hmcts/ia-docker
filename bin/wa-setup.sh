2d3b9d95263774a975e162657db71f036a5a62f2
ThomasKKC <thomas.cheong@version1.com>
2022-10-19 16:34:24 +0100

    RIA-6373 Init commit

--- bin/wa-setup.sh
+++ bin/wa-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export ROLE_ASSIGNMENT_URL=http://localhost:4096
+export ROLE_ASSIGNMENT_URL=http://am-role-assignment-service:4096
 export CCD_URL=http://ccd-data-store-api:4452
 # Setup Users
 ./wa-create-users.sh
